be0964002ade072aa04e7078baea9e2c575313d2
Alexey Khripko <alexey.khripko@hp.com>
2015-06-18 16:27:30 +0300

    module & makeArmy examples

---
+++ js2.js
@@ -0,0 +1,44 @@
+
+// module
+var myModule = (function (moduleName) {
+
+    function printModuleName() {
+        console.log(moduleName);
+    }
+
+    function foo(){
+        console.log("foo of " + moduleName + " was called");
+    }
+
+    console.log(moduleName + "was created");
+
+    return {
+        printModuleName : printModuleName,
+        foo : foo
+    }
+
+})("myModule");
+
+
+myModule.printModuleName();
+myModule.foo();
+
+
+// two solutions of it:
+function makeArmy() {
+    var shooters = [], i;
+    for (i = 0; i < 10; i++) (function (k) {
+        var shooter = function () {
+            console.log(k);
+            console.log(shooter.savedValue);
+        };
+        shooter.savedValue = i;
+        shooters.push(shooter);
+    })(i);
+    return shooters;
+}
+
+var army = makeArmy();
+
+army[0]();
+army[5]();
